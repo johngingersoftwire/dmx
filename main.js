bc9053302a7c33184dd1921675cdf4688827679c
John Ginger <john.ginger@softwire.com>
2015-12-08 20:49:40 +0000

    initial

---
+++ main.js
@@ -0,0 +1,27 @@
+var artnetOptions = {
+    host: '10.213.3.163'
+};
+
+var artnet = require('artnet')(artnetOptions);
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.send('I am an api!');
+});
+
+app.post('/', function(request, response){
+    console.log(request.body.dmx);      // your JSON
+    response.send(request.body.dmx);    // echo the result back
+    artnet.set(request.body.dmx);
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
